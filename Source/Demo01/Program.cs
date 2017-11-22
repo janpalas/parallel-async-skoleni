12fd6a958186dbc2fec4d52780a439a2a5f5f086
Jan Palas <jan.palas@gmail.com>
2017-11-22 11:15:47 +0100

    ThreadPool

--- Source/Demo01/Program.cs
+++ Source/Demo01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Demo01
@@ -11,15 +12,35 @@ namespace Demo01
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-           for(int i = 0; i < int.MaxValue; i++)
-           {
-                Console.WriteLine(i);
-                var t = new Thread(o =>
-                {
-                    Thread.Sleep(-1);
-                    //Console.WriteLine(i);
-                    //Thread.Sleep(int.MaxValue);
-                });
+            TestThreadPool();
+
+            Console.ReadLine();
+        }
+
+        private static void TestThreadPool()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                //ThreadPool si resi vytvareni vlaken sam, nejdrive kazdych 500 ms vyhodnocuje, jestli ma aplikace dostatek vlaken.
+                //Pokud ne, tak prida dalsi, pokud jich je prebytek, tak zase ubere
+                ThreadPool.QueueUserWorkItem(o => { Thread.Sleep(-1); }, null);
+
+                //Thread.Sleep(0) = Thread.Yield = vzdavam se procesoru, ale ten mi muze byt hned zase pridelen
+                //Thread.Sleep(16) - to je minimalni rozliseni procesoru (16 = 1sec./64Hz = 1000 ms / 64 = 15.625)
+            }
+            Console.WriteLine("Done");
+
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine(sw.Elapsed);
+            Thread.Sleep(1);
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static void TestMaxThreads()
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var t = new Thread(o => { Thread.Sleep(-1); });
 
                 //Pokud chceme jednomu vlaknu zvysit prioritu, tak je lepsi vsem ostatnim vlaknum
                 //prioritu snizit, nez jednomu zvysit
@@ -27,9 +48,8 @@ namespace Demo01
                 t.Name = "Testovaci-vlakno";
 
                 t.Start();
+                Console.WriteLine(i);
             }
-
-            Console.ReadLine();
         }
     }
 }

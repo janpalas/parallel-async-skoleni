ea0dc0d610725f23cdff8c6c9fe0ea72bad63c8d
Honza <jan.palas@aginfo.cz>
2017-11-23 22:41:22 +0100

    WpfTest app for exceptions

---
+++ Source/WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfTestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Task.Run(() => throw new Exception("TEST"));
+		}
+
+		private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+		{
+			MessageBox.Show(e.Exception.GetBaseException().ToString());
+			e.SetObserved();
+		}
+
+		private void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show(e.Exception.ToString());
+			e.Handled = true;
+		}
+	}
+}
